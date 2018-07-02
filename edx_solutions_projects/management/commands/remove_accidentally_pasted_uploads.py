81251ac715047053409df1b4445c0c839a3e8e61
Kshitij Sobti <kshitij@sobti.in>
2018-07-02 20:45:39 +0530

    Add management command to clean up unneeded image.png submissions

---
+++ edx_solutions_projects/management/commands/remove_accidentally_pasted_uploads.py
@@ -0,0 +1,30 @@
+"""
+Management command to remove spurious workgroup submissions of image.png files
+created due to an issue in xblock-group-project-v2 before v0.4.9 where it would
+upload the image component of anything copied into the clipboard.
+"""
+import logging
+from django.core.management.base import BaseCommand
+from edx_solutions_projects.models import WorkgroupSubmission
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Removes spurious spurious workgroup submissions containing "image.png" due
+    to an issue in xblock-group-project-v2 < v0.4.9
+    """
+    help = 'Removes spurious workgroup submissions containing "image.png"'
+
+    def handle(self, *args, **options):
+        log.info('Deleting workgroup submissions containing a document called "image.png"')
+
+        choice = raw_input('Are you sure? [yn]').lower()
+        if choice == 'y' or choice == 'yes':
+            WorkgroupSubmission.objects.filter(
+                document_filename='image.png',
+                document_id=u'Kickoff_deliverable',
+            ).delete()
+
+            log.info('Done.')
