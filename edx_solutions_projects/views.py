df305817e7de7730688f13781f4e2cfa33e2bdbe
Nasir Hussain <nasirhjafri@gmail.com>
2020-06-12 13:26:35 +0500

    MCKIN-18950 Added organization in groupwork detailed API

--- edx_solutions_projects/views.py
+++ edx_solutions_projects/views.py
@@ -316,7 +316,7 @@ class ProjectsViewSet(SecureModelViewSet):
             serializer_cls = WorkgroupSerializer
             if 'details' in request.query_params:
                 serializer_cls = WorkgroupDetailsSerializer
-                workgroups = workgroups.prefetch_related('submissions')
+                workgroups.prefetch_related('submissions', 'users', 'users__organizations')
             response_data = []
             if workgroups:
                 for workgroup in workgroups:
