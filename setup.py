c9f0f2b15bcbe93d0f17210d1eb876dc38fc1053
mudassir hafeez <mudassir.hafeez@arbisoft.com>
2020-07-29 17:18:35 +0000

    version 2.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='projects-edx-platform-extensions',
-    version='1.1.16',
+    version='2.0.4',
     description='Projects management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
